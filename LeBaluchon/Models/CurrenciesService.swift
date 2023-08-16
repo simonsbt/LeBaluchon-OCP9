0035899d7c0eb787d806a5ee28d424ec0a3e4aa0
Simon Sabatier <74857456+simonsbt@users.noreply.github.com>
2023-08-16 11:20:30 +0200

    Update CurrenciesService.swift

--- LeBaluchon/Models/CurrenciesService.swift
+++ LeBaluchon/Models/CurrenciesService.swift
@@ -8,29 +8,45 @@
 import Foundation
 
 class CurrenciesService {
+
+    private var task: URLSessionDataTask?
+
+    static var shared = CurrenciesService()
     
     let currencies = ["USD", "GBP", "JPY", "CAD"]
+    
     private static let baseCurrency = "base=EUR"
     private static let targetCurrencies = "symbols=USD,GBP,JPY,CAD" /* https://fr.wikipedia.org/wiki/ISO_4217#Liste_triée_par_nom_d’unité_monétaire */
     private static let apikey = "apikey=xxxxxxxxxx"
-    private let currenciesUrl = URL(string: "https://api.apilayer.com/fixer/latest?" + apikey + "&" + baseCurrency + "&" + targetCurrencies)!
-    private var currenciesResponse = CurrenciesResponse()
     
     func getCurrenciesRates() {
-        print("enter in the getCurrenciesRates func")
+        
+        let currenciesUrl = URL(string: "https://api.apilayer.com/fixer/latest?\(apikey)&\(baseCurrency)&\(targetCurrencies)")!
+        
         let session = URLSession(configuration: .default)
-        let task = session.dataTask(with: currenciesUrl) { (data, response, error) in
-            guard let data = data, error == nil else {
-                print("error")
-                return
-            }
-            
-            if let decodedResponse = try? JSONDecoder().decode(CurrenciesResponse.self, from: data) {
-                print("Response decoded successfully")
-                self.currenciesResponse = decodedResponse
-                self.currenciesResponse.saveCurrenciesRates()
+        
+        task = session.dataTask(with: currenciesUrl) { (data, response, error) in
+
+            DispatchQueue.main.async {
+                guard let data = data, error == nil else {
+                    callback(false, nil)
+                    return
+                }
+
+                guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                    callback(false, nil)
+                    return
+                }
+
+                guard let decodedResponse = try? JSONDecoder().decode(CurrenciesResponse.self, from: data) else {
+                    callback(false, nil)
+                    return
+                }
+
+                let currenciesResponse: CurrenciesResponse = decodedResponse
+                currenciesResponse.saveCurrenciesRates()
             }
         }
-        task.resume()
+        task.resume()!
     }
 }
